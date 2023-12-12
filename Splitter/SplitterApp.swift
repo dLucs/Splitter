b9059a9a52458f2b2b29eedf5f678eb03f2ab410
dLucs <lucas_61@hotmail.es>
2023-12-12 12:10:23 +0100

    Initial Commit

---
+++ Splitter/SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitterApp.swift
+//  Splitter
+//
+//  Created by Lucas on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
