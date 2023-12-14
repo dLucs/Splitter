c4b0c03456108b15101fe866a3c0b70b9399f4fe
dLucs <lucas_61@hotmail.es>
2023-12-14 14:00:30 +0100

    Added all the UI elements and interactions

--- Splitter/ContentView.swift
+++ Splitter/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    let tipPercentages =  [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let total = checkAmount + tipValue
+        let amountPerPerson = total / peopleCount
+        
+        return amountPerPerson
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section("Add check amount & number of people"){
+                    TextField("Amount", value: $checkAmount, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                Section("How much do you want to tip?"){
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section("Amount per person"){
+                    Text(totalPerPerson, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("Splitter")
+            .toolbar {
+                    if amountIsFocused{
+                        Button("Done"){amountIsFocused = false}                   }
+                }
         }
-        .padding()
     }
 }
 
